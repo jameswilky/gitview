4ccd84a8fcf9bbab49ed2cb85be26af7afb44670
James Wilkinson <jdwilkinson1994@gmail.com>
2019-01-11 16:14:14 +0800

    fixed client secret thing

--- app.js
+++ app.js
@@ -6,30 +6,80 @@ const ui = new UI;
 // Search input
 const searchUser = document.getElementById('searchUser');
 
+// Testing URL
+// https://github.com/Gethe/wow-ui-textures
+
+const traverseRepo = function (repo) {
+  let limit = 5
+  let count = 0;
+  repo.forEach(file => {
+    if (file.type == "file") {
+      // Display file 
+    }
+    else if (file.type == "dir") {
+      // Open folder
+      console.log(file)
+      const url = file.url
+      if (count < limit) {
+        github.openDir(url)
+          .then(data => {
+            //recursively call function untill no directories left
+            // console.log('called recursively')
+            // traverseRepo(data.dir)
+          })
+          .catch(err => {
+            console.log("Error Found", err)
+          })
+        count += 1
+
+      }
+      else {
+        console.log("Custom Limit Reached")
+      }
+
+
+    }
+  });
+}
+
 // Search input event listener
-searchUser.addEventListener('keyup', (e) => {
-  // Get input text
-  const userText = e.target.value;
+searchUser.addEventListener('keypress', (e) => {
+  const key = e.which || e.keyCode;
+  if (key === 13) { // 13 is enter
+    // Get input text
+    let targetRepo = e.target.value;
+    targetRepo = "https://github.com/Gethe/wow-ui-textures"
 
-  if (userText !== '') {
-    // Make http call
+    //Validate that format is correct
+    let valid = true //temp
 
-    github.getUser(userText)
-      .then(data => {
-        if (data.profile.message == 'Not Found') {
-          // Show alert
-          ui.showAlert('User not Found', 'alert alert-danger');
+    if (valid) {
+      github.getRepo(targetRepo)
+        .then(data => {
+          traverseRepo(data.repo)
+        })
 
-        } else {
-          // Show profile
-          console.log(data)
-          ui.showProfile(data.profile);
-          ui.showRepos(data.repos)
-        }
-      })
+    }
   }
-  else {
-    //Clear profile
-    ui.clearProfile();
-  }
+
+  // if (userText !== '') {
+  //   // Make http call
+
+  //   github.getUser(userText)
+  //     .then(data => {
+  //       if (data.profile.message == 'Not Found') {
+  //         // Show alert
+  //         ui.showAlert('User not Found', 'alert alert-danger');
+
+  //       } else {
+  //         // Show profile
+  //         ui.showProfile(data.profile);
+  //         ui.showRepos(data.repos)
+  //       }
+  //     })
+  // }
+  // else {
+  //   //Clear profile
+  //   ui.clearProfile();
+  // }
 })
--- github.js
+++ github.js
@@ -1,24 +1,43 @@
 class Github {
   constructor() {
-    this.client_id = '8378f3d58cd1f204c19f';
-    this.client_secret = '7f0ffd186562109772bd8fb25c81554c1ccc50b7';
+    this.client_id = '8f1cd47c8b186a34c542';
+    this.client_secret = '60a0047b85b25e76e28548a46860bc222be8a255';
     this.repos_count = 5;
     this.repos_sort = 'created: asc'
   }
 
-  async getUser(user) {
-    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`)
+  async getRepo(url) {
+    const tokens = url.split("/")
+    const user = tokens[3]
+    const repo_name = tokens[4]
+    const repoResponse = await fetch(`https://api.github.com/repos/${user}/${repo_name}/contents?client_id=${this.client_id}&client_secret=${this.client_secret}`)
 
-
-    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_pafge=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`)
-
-    const profile = await profileResponse.json();
-    const repos = await repoResponse.json();
-
-
+    const repo = await repoResponse.json();
     return {
-      profile,
-      repos
+      repo
     }
   }
+
+  async openDir(url) {
+    const dirResponse = await fetch(`${url}&client_id=${this.client_id}&client_secret=${this.client_secret}`)
+    const dir = await dirResponse.json()
+
+    return {
+      dir
+    }
+  }
+
+  // async getUser(user) {
+  //   const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`)
+  //   const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`)
+
+  //   const profile = await profileResponse.json();
+  //   const repos = await repoResponse.json();
+
+
+  //   return {
+  //     profile,
+  //     repos
+  //   }
+  // }
 }
